b4aa195f70fe5daa67cbf13897c9185c27f2f187
Assem-944 <elghemariassem3@gmail.com>
2023-03-16 16:43:22 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 /**
- * main - Entry point 
- * Return: always 0 (success)
+ * main - Entry point of the program
+ *
+ * This program demonstrates the use of printf function
+ * to display the size of the type of variable
+ * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
 printf("Size of a char: %zu byte(s)\n", sizeof(char));
 printf("Size of an int: %zu byte(s)\n", sizeof(int));
-printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long));
 printf("Size of a float: %zu byte(s)\n", sizeof(float));
 return (0);
 }
