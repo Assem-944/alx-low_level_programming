eb447b242dca87918b0353ec7c695e22083d3d22
Assem-944 <elghemariassem3@gmail.com>
2023-03-17 17:17:38 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,40 +1,17 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - print numbers from 00 to 99.
-(*
- * Return: 0 on success
- */
+ * main - Print out the alphabet in lowercase
+ (*
+  * Return: 0 (Success)
+  */
 int main(void)
 {
-	int i = '0';
-	int j = '0';
-	int k = '0';
+	char c = 'a';
 
-	while (i <= '7')
+	while (c <= 'z')
 	{
-		while (j <= '8')
-		{
-			while (k <= '9')
-			{
-				if (i < j && j < k)
-				{
-					putchar(i);
-					putchar(j);
-					putchar(k);
-					if (!(i == '7' && j == '8' && k == '9'))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-
-				}
-				k++;
-			}
-			k = '0';
-			j++;
-		}
-		j = '0';
-		i++;
+		putchar(c);
+		c++;
 	}
 	putchar('\n');
 	return (0);
